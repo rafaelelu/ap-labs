13c68a1b079ee87be6b7b738d9c589e67f8b0272
Rafael Elu <a01634309@itesm.mx>
2019-09-05 17:56:02 -0500

    Add c-basics
    Signed-off-by: Rafael Elu <a01634309@itesm.mx>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,17 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
-
-/* print Fahrenheit-Celsius table */
-
-int main()
+int main(int argc, char **argv)
 {
-    int fahr;
+    int LOWER = 0;
+    int UPPER = 300;
+    int STEP = 20;
 
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    switch(argc){
+        case 2:
+            LOWER = atoi(argv[1]);
+            UPPER = LOWER;
+            break;
+        case 4:
+            LOWER = atoi(argv[1]);
+            UPPER = atoi(argv[2]);
+            STEP = atoi(argv[3]);
+            break;
+        default:
+            printf("You need to send either the fahrenheit degrees to convert or the start, end and increment.\n");
+            printf("How to execute: ./fahrenheit_celsius <num_farenheit_degrees>\n");
+            printf("OR\n");
+            printf("./fahrenheit_celsius <start> <end> <increment>\n");
+            return 1;
+    }
+
+    int fahr;
+    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP){
+	    printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+    }
 
     return 0;
 }
