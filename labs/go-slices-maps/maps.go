ccc34aae4a851a33a7019716d3e0e1fd9b4b2d77
Rafael Elu <a01634309@itesm.mx>
2019-09-10 21:18:03 -0500

    Add slices.go and maps.go
    Signed-off-by: Rafael Elu <a01634309@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,19 @@
 package main
 
-func main() {
+import (
+	"golang.org/x/tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	m := make(map[string]int);
+	for _, e:= range strings.Fields(s){
+		m[string(e)] += 1;
+	}
+	return m;
 }
+
+func main() {
+	wc.Test(WordCount)
+}
+
