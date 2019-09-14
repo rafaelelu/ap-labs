b648177467eb1709d26112f258970e6024b9b259
Rafael Elu <a01634309@itesm.mx>
2019-09-14 11:27:09 -0500

    Add strlib.c
    Signed-off-by: Rafael Elu <a01634309@itesm.mx>

--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,50 @@
+int mystrlen(char *str);
+char *mystradd(char *origin, char *addition);
+int mystrfind(char *origin, char *substr);
 
 
 int mystrlen(char *str){
-    return 0;
+    int i = 0;
+    while(str[i] != '\0'){
+        i++;
+    }
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int lengthOfNewString;
+    int lengthOfOrigin = mystrlen(origin);
+    int lengthOfAddition = mystrlen(addition);
+    lengthOfNewString =  lengthOfOrigin + lengthOfAddition + 1;
+    char newString[lengthOfNewString];
+    int i = 0;
+    for(int j = 0; j < lengthOfOrigin; j++){
+        newString[i] = origin[j];
+        i++;
+    }
+    for(int k = 0; k < lengthOfAddition; k++){
+        newString[i] = addition[k];
+        i++;
+    }
+    newString[i] = '\0';
+    return newString;
 }
 
 int mystrfind(char *origin, char *substr){
+    int originLen = mystrlen(origin);
+    int substrLen = mystrlen(substr);
+    int j = 0;
+    for(int i = 0; i <= originLen; i++){
+        if(origin[i] == substr[0] && j < 1){
+            j++;
+        } else if(origin[i] == substr[j]){
+            j++;
+        } else {
+            j = 0;
+        }
+        if(j >= substrLen){
+            return 1;
+        }
+    }
     return 0;
 }
