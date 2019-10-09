9ab4eaeb46bfc771145c8d2a40e5bc83082ab165
Rafael Elu <a01634309@itesm.mx>
2019-10-08 23:10:10 -0500

    Added logger.c testLogger.c
    Signed-off-by: Rafael Elu <a01634309@itesm.mx>

---
+++ labs/logger/logger.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/types.h> 
+#include <unistd.h> 
+
+#define RESET		0
+#define BRIGHT 		1
+#define DIM		    2
+#define UNDERLINE 	3
+#define BLINK		4
+#define REVERSE		7
+#define HIDDEN		8
+
+#define BLACK 		0
+#define RED		    1
+#define GREEN		2
+#define YELLOW		3
+#define BLUE		4
+#define MAGENTA		5
+#define CYAN		6
+#define	WHITE		7
+
+void textcolor(int attr, int fg, int bg)
+{	char command[13];
+
+	/* Command is the control command to the terminal */
+	sprintf(command, "%c[%d;%d;%dm", 0x1B, attr, fg + 30, bg + 40);
+	printf("%s", command);
+}
+
+
+int infof(const char *format, ...) {
+
+    int done;
+    va_list arg;
+	va_start (arg, format);
+    textcolor(BRIGHT, WHITE, BLACK);
+    printf("INFO: ");
+    done = vfprintf (stdout, format, arg);
+    va_end (arg);
+    textcolor(RESET, GREEN, BLACK);	
+    return done;
+
+}
+
+int warnf(const char *format, ...) {
+
+    int done;
+    va_list arg;
+	va_start (arg, format);
+    textcolor(BRIGHT, YELLOW, BLACK);
+    printf("WARNING: ");
+    done = vfprintf (stdout, format, arg);
+    va_end (arg);
+    textcolor(RESET, GREEN, BLACK);	
+    return done;
+
+}
+
+int errorf(const char *format, ...) {
+    
+    int done;
+    va_list arg;
+	va_start (arg, format);
+    textcolor(BRIGHT, RED, BLACK);
+    printf("ERROR: ");
+    done = vfprintf (stdout, format, arg);
+    va_end (arg);
+    textcolor(RESET, GREEN, BLACK);	
+    return done;
+
+}
+
+int panicf(const char *format, ...) {
+    
+    int done;
+    va_list arg;
+	va_start (arg, format);
+    textcolor(BRIGHT, GREEN, BLACK);
+    printf("PANIC: ");
+    done = vfprintf (stdout, format, arg);
+    va_end (arg);
+    textcolor(RESET, GREEN, BLACK);
+    kill(getpid(), SIGQUIT);
+    return done;
+
+}
--- labs/logger/testLogger.c
+++ labs/logger/testLogger.c
@@ -1,5 +1,11 @@
-
+# include "logger.c"
 
 int main() {
+
+    infof("info: %d\n", 7);
+    warnf("warning %c\n", 'a');
+    errorf("error %f\n", 7.0f);
+    panicf("panic\n");
+
     return 0;
 }
