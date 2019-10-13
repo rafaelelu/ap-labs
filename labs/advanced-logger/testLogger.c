4345ad0c6355ca2c49352a24bf983e5605df2565
Rafael Elu <a01634309@itesm.mx>
2019-10-12 19:12:31 -0500

    Added logger.c logger.h testLogger.c
    Signed-off-by: Rafael Elu <a01634309@itesm.mx>

--- labs/advanced-logger/testLogger.c
+++ labs/advanced-logger/testLogger.c
@@ -4,15 +4,15 @@
 int main(){
 
     // default logging
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
+    infof("INFO Message %d\n", 1);
+    warnf("WARN Message %d\n", 2);
+    errorf("ERROR Message %d\n", 2);
 
     // stdout logging
     initLogger("stdout");
-    infof("INFO Message %d", 1);
-    warnf("WARN Message %d", 2);
-    errorf("ERROR Message %d", 2);
+    infof("INFO Message %d\n", 1);
+    warnf("WARN Message %d\n", 2);
+    errorf("ERROR Message %d\n", 2);
 
     // syslog logging
     initLogger("syslog");
