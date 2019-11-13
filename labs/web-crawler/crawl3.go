ae3588780a13d15d00821632826616f92691dec9
Rafael Elu <a01634309@itesm.mx>
2019-11-12 23:40:40 -0600

    Added crawl3.go
    Signed-off-by: Rafael Elu <a01634309@itesm.mx>

--- labs/web-crawler/crawl3.go
+++ labs/web-crawler/crawl3.go
@@ -16,16 +16,23 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"strconv"
 
-	"gopl.io/ch5/links"
+	"github.com/todostreaming/gopl.io/ch5/links"
 )
+
+type paths struct {
+	urls []string
+	father int
+}
 
 //!+sema
 // tokens is a counting semaphore used to
 // enforce a limit of 20 concurrent requests.
 var tokens = make(chan struct{}, 20)
 
-func crawl(url string) []string {
+func crawl(url string, f int) paths {
 	fmt.Println(url)
 	tokens <- struct{}{} // acquire a token
 	list, err := links.Extract(url)
@@ -34,31 +41,55 @@ func crawl(url string) []string {
 	if err != nil {
 		log.Print(err)
 	}
-	return list
+	pathsToReturn := paths{urls: list, father: f+1}
+	return pathsToReturn
 }
 
 //!-sema
 
 //!+
 func main() {
-	worklist := make(chan []string)
+	if len(os.Args) < 3 {
+		log.Fatalf("How to execute: go run crawl3.go -depth=[int] [url]\n")
+	}
+
+	checkFlag := strings.Split(os.Args[1], "=")[0]
+	if (checkFlag != "-depth") {
+		log.Fatalf("depth should be an integer greater than zero")
+	}
+
+	depthString := strings.Split(os.Args[1], "=")[1]
+	depth, err := strconv.Atoi(depthString)
+	if err != nil {
+		log.Fatalf("depth should be an integer greater than zero")
+	}
+
+
+
+	worklist := make(chan paths)
 	var n int // number of pending sends to worklist
 
 	// Start with the command-line arguments.
 	n++
-	go func() { worklist <- os.Args[1:] }()
+	firstpath := paths{urls: os.Args[2:], father: 1}
+
+	go func() { worklist <- firstpath}()
 
 	// Crawl the web concurrently.
 	seen := make(map[string]bool)
 	for ; n > 0; n-- {
-		list := <-worklist
-		for _, link := range list {
+		p := <-worklist
+		for _, link := range p.urls {
 			if !seen[link] {
 				seen[link] = true
 				n++
-				go func(link string) {
-					worklist <- crawl(link)
-				}(link)
+				if p.father <= depth {
+					go func(link string) {
+						worklist <- crawl(link, p.father)
+					}(link)
+				} else {
+					return
+				}
 			}
 		}
 	}
